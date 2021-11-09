b197ddbe52b6c249c7ff0a0848bd6df369251750
andriykhomik <andriykhomik1992@gmail.com>
2021-11-09 22:49:46 +0200

    created this branch and change function 'getJoke' to an 'async-await syntax

--- main.js
+++ main.js
@@ -8,19 +8,35 @@ getJoke();
 
 btn.addEventListener('click', getJoke);
 
-function getJoke(){
+// function getJoke(){
+//     const config = {
+//         headers: {
+//             Accept: 'application/json'
+//         }
+//     }
+//
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(response=> response.json())
+//         .then(joke => jokeEl.innerText = joke.joke)
+//         .catch(err => {
+//             throw(err)
+//         })
+// }
+
+async function getJoke(){
     const config = {
         headers: {
             Accept: 'application/json'
         }
     }
+    try {
+        const response = await fetch('https://icanhazdadjoke.com', config);
+        const joke = await response.json();
+        jokeEl.innerText = joke.joke;
+    } catch (e){
+        console.log (e);
+    }
 
-    fetch('https://icanhazdadjoke.com', config)
-        .then(response=> response.json())
-        .then(joke => jokeEl.innerText = joke.joke)
-        .catch(err => {
-            throw(err)
-        })
+
 }
-
 
